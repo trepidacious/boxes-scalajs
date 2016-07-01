920f28aee59931eae24cdcca1006286b58d05be9
trepidacious <trepidacious@gmail.com>
2016-07-01 20:25:51 +0100

    Set up build and demo app

---
+++ src/main/scala/org/rebeam/boxes/scalajs/demo/DemoApp.scala
@@ -0,0 +1,9 @@
+package org.rebeam.boxes.scalajs.demo
+
+import scala.scalajs.js.JSApp
+
+object DemoApp extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
---
+++ src/main/scala/org/rebeam/boxes/scalajs/demo/build.sbt
@@ -0,0 +1,8 @@
+enablePlugins(ScalaJSPlugin)
+
+name := "boxes-scalajs"
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
